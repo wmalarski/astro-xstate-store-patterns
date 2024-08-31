4b0395657e755dccc18f6e05c8c77c5ef49a23ad
Wojciech Malarski <w.malarski@gmail.com>
2024-08-31 14:42:32 +0200

    adds cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [
@@ -15,4 +17,6 @@ export default defineConfig({
     }),
     tailwind(),
   ],
+  output: "static",
+  adapter: cloudflare(),
 });
